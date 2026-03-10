d12c26a1dd74ade959cb79418c2cb0a5a4a61976
Rodrigo <Gutierrezrodrigo1709@gmail.com>
2026-03-10 11:22:10 -0400

    :q :w

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/available', function () {
+    return('<h1> Available </h1>');
+}) -> name('Available');
+
+Route::any('/submit', function () {
+    return 'submited';
+});
+
+Route::get('/test', function () {
+    $url = Route('Available');
+    return "<a href= '$url'> Click Here" ;
+});
+
+Route::get('/api/user', function(){
+    return [
+        'name' =>'Rodrigo Gutierrez',
+        'email' => 'Rodrigo@gmail.com',
+    ];
+});
